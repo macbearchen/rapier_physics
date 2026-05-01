53a6c046a702c81481ddceee955f1d4bfd03324a
macbearchen <5847542+macbearchen@users.noreply.github.com>
2026-05-01 05:55:23 +0000

    Deploying to gh-pages from @ macbearchen/rapier_physics@cfe1a7af681db461442a5db0c82bc7f7a943137f 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2308488342" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "977655564" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
